08a10c3f5a0cc402b329dd8d8a239e28c4a9e25e
ambMike <mgiammattei@ambrosiatc.com>
2017-11-18 11:44:53 -0500

    Added sass core files.

--- content/themes/telavice/includes/config.php
+++ content/themes/telavice/includes/config.php
@@ -1,4 +1,5 @@
 <?php
 
 /** functions for to include in the head section of the code  */
-define("THEME_DIR", get_template_directory_uri());
+define("THEME_DIR_URL", get_template_directory_uri());
+define("THEME_DIR", get_template_directory());
