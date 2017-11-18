4ae2603e1ba5224677023f6b1fa52eff72040c58
ambMike <mgiammattei@ambrosiatc.com>
2017-11-18 10:04:07 -0500

    Initialized the new custom theme.

---
+++ content/themes/telavice/functions.php
@@ -0,0 +1,4 @@
+<?php
+
+/** Remove unused and adjust wp core files */
+require_once( __DIR__ . '/includes/core-adjustments.php');
---
+++ content/themes/telavice/includes/core-adjustments.php
@@ -0,0 +1,47 @@
+<?php
+// REMOVE EMOJI ICONS
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+
+//** Removes resource file that comes prebuilt with WordPress. */
+remove_action('wp_head', 'wp_resource_hints', 2);
+
+//** Removes the json file that comes prebuilt with WordPress. */
+function remove_json_api () {
+
+	// Remove the REST API lines from the HTML Header
+	remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
+	remove_action( 'wp_head', 'wp_oembed_add_discovery_links', 10 );
+
+	// Remove the REST API endpoint.
+	remove_action( 'rest_api_init', 'wp_oembed_register_route' );
+
+	// Turn off oEmbed auto discovery.
+	add_filter( 'embed_oembed_discover', '__return_false' );
+
+	// Don't filter oEmbed results.
+	remove_filter( 'oembed_dataparse', 'wp_filter_oembed_result', 10 );
+
+	// Remove oEmbed discovery links.
+	remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
+
+	// Remove oEmbed-specific JavaScript from the front-end and back-end.
+	remove_action( 'wp_head', 'wp_oembed_add_host_js' );
+
+	// Remove all embeds rewrite rules.
+	add_filter( 'rewrite_rules_array', 'disable_embeds_rewrites' );
+
+}
+add_action( 'after_setup_theme', 'remove_json_api' );
+
+/** Puts the jquery scripts to the footer.  */
+function remove_head_scripts() {
+	remove_action('wp_head', 'wp_print_scripts');
+	remove_action('wp_head', 'wp_print_head_scripts', 9);
+	remove_action('wp_head', 'wp_enqueue_scripts', 1);
+
+	add_action('wp_footer', 'wp_print_scripts', 5);
+	add_action('wp_footer', 'wp_enqueue_scripts', 5);
+	add_action('wp_footer', 'wp_print_head_scripts', 5);
+}
+add_action( 'wp_enqueue_scripts', 'remove_head_scripts' );
